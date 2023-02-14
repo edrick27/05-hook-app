927ac378c0c55c510cfbe39c47fd76318595b1da
edrick <edricklpcr@gmail.com>
2023-02-13 19:34:32 -0600

    add ballback hook

--- src/06-memos/Memorize.jsx
+++ src/06-memos/Memorize.jsx
@@ -4,27 +4,27 @@ import { Small } from "./Small";
 
 export const Memorize = () => {
 
-    const {counter, increment } = useCounter(2);
+    const { counter, increment } = useCounter(2);
 
     const [Show, setShow] = useState(true)
 
-  return (
-    <>
-        <h1>Counter <Small value={counter} /></h1>
+    return (
+        <>
+            <h1>Counter <Small value={counter} /></h1>
 
-        <button
-        className="btn btn-primary"
-        onClick={() => increment(1)}>
-            +1
-        </button>
+            <button
+                className="btn btn-primary"
+                onClick={() => increment(1)}>
+                +1
+            </button>
 
-        <button
-        className="btn btn-info"
-        onClick={() => {
-            setShow(!Show);
-        }}>
-            Show/Hide {JSON.stringify(Show)}
-        </button>
-    </>
-  )
+            <button
+                className="btn btn-info"
+                onClick={() => {
+                    setShow(!Show);
+                }}>
+                Show/Hide {JSON.stringify(Show)}
+            </button>
+        </>
+    )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-userRef/FocusScreen'
 import { Layout } from './05-useLayoutEffet/Layout'
+import { CallbackHook } from './06-memos/CallbackHook'
 import { MemorizeHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
+import { Padre } from './07-tarea-memo/Padre'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemorizeHook/>
+    <Padre/>
   </React.StrictMode>,
 )
